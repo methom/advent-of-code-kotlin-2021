d514a96df3fc5c11586bd09a0a8945c3184c1004
Merlin Thomas <emteeware@gmail.com>
2021-12-01 20:10:33 +0100

    Add solution for day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
+    val dayNumber = "01"
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return (1 until input.size).count { input[it].toInt() > input[it - 1].toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return (3 until input.size).count { (input[it].toInt() + input[it - 1].toInt() + input[it - 2].toInt()) > (input[it - 1].toInt() + input[it - 2].toInt() + input[it - 3].toInt()) }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day${dayNumber}_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day${dayNumber}")
     println(part1(input))
     println(part2(input))
 }
