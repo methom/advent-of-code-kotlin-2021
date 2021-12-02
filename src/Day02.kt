2fa5b7c1e28969f0e4be76f93e28c450c1eb0427
Merlin Thomas <emteeware@gmail.com>
2021-12-02 19:20:15 +0100

    Add solution for day 02, part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -17,12 +17,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val location = Location()
+        var aim = 0
+        input.forEach {
+            when(Direction.valueOf(it.split(' ')[0].uppercase())) {
+                Direction.UP -> aim -= it.split(' ')[1].toInt()
+                Direction.DOWN -> aim += it.split(' ')[1].toInt()
+                Direction.FORWARD -> {
+                    val inc = it.split(' ')[1].toInt()
+                    location.depth += (aim * inc)
+                    location.horizontal += inc
+                }
+            }
+        }
+        return location.horizontal * location.depth
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${dayNumber}_test")
     check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day${dayNumber}")
     println(part1(input))
