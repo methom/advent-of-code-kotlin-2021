5f998c89e3da25bbdaff33a341f66d05e15d27be
Merlin Thomas <emteeware@gmail.com>
2021-12-05 11:11:59 +0100

    Add solution for day 05, part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -4,24 +4,33 @@ import java.lang.Integer.min
 fun main() {
     val dayNumber = "05"
 
-    fun part1(input: List<String>): Int {
+    fun initCoordMap(input: List<String>): CoordMap {
         val ventMap = CoordMap()
         val regex = "\\D+".toRegex()
         input.forEach {
             val coords = it.split(regex)
             ventMap.addVentLine(coords)
         }
+        return ventMap
+    }
+
+    fun part1(input: List<String>): Int {
+        val ventMap = initCoordMap(input)
         return ventMap.countMostDangerousAreas()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val ventMap = initCoordMap(input)
+        return ventMap.countMostDangerousAreas(includeDiagonals = true)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${dayNumber}_test")
     check(part1(testInput) == 5)
-    check(part2(testInput) == 10)
+    check(part2(testInput) == 12)
+
+//    val testBInput = readInput("Day05_testB")
+//    check(part2(testBInput) == 0)
 
     val input = readInput("Day${dayNumber}")
     println("Part 1 answer: ${part1(input)}")
@@ -55,8 +64,8 @@ class CoordMap() {
         }
     }
 
-    fun printMap() {
-        val map = initMap()
+    fun printMap(includeDiagonals: Boolean = false) {
+        val map = initMap(includeDiagonals)
         map.forEach {
             it.forEach { value ->
                 print(if (value == 0) ". " else "$value ")
@@ -65,7 +74,7 @@ class CoordMap() {
         }
     }
 
-    private fun initMap(): Array<IntArray> {
+    private fun initMap(includeDiagonals: Boolean = false): Array<IntArray> {
         val map = Array(maxY + 1) { IntArray(maxX + 1) { 0 } }
         ventLineList.forEach {
             if (it.startingPoint.first == it.endingPoint.first) {
@@ -84,13 +93,47 @@ class CoordMap() {
                 for (x in start..end) {
                     map[y][x]++
                 }
+            } else if (includeDiagonals) {
+                // draw diagonal lines
+                val x1 = it.startingPoint.first
+                val x2 = it.endingPoint.first
+                val y1 = it.startingPoint.second
+                val y2 = it.endingPoint.second
+                val lineLength = max(x2,x1) - min(x2,x1)
+                if (x2 > x1) {
+                    // rightward diagonal
+                    if (y2 > y1) {
+                        // downward diagonal
+                        for (i in 0 .. lineLength) {
+                            map[y1 + i][x1 + i]++
+                        }
+                    } else {
+                        // upward diagonal
+                        for (i in 0 .. lineLength) {
+                            map[y1 - i][x1 + i]++
+                        }
+                    }
+                } else {
+                    // leftward diagonal
+                    if (y2 > y1) {
+                        // downward diagonal
+                        for (i in 0 .. lineLength) {
+                            map[y1 + i][x1 - i]++
+                        }
+                    } else {
+                        // upward diagonal
+                        for (i in 0 .. lineLength) {
+                            map[y1 - i][x1 - i]++
+                        }
+                    }
+                }
             }
         }
         return map
     }
 
-    fun countMostDangerousAreas(): Int {
-        val map = initMap()
+    fun countMostDangerousAreas(includeDiagonals: Boolean = false): Int {
+        val map = initMap(includeDiagonals)
         var numberOfDangerousAreas = 0
         map.forEach {
             it.forEach { value ->
