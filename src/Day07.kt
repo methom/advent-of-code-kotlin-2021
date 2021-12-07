f3233c344bc4679d3c3291d313d9ed07b9a0a96a
Merlin Thomas <emteeware@gmail.com>
2021-12-07 19:43:45 +0100

    Add solution for day 07, part 2
    Unfortunately, this is a brute force solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     val dayNumber = "07"
 
@@ -9,18 +11,36 @@ fun main() {
             (positions[(positions.size - 1)/2] + positions[(positions.size + 1)/2])/2
         }
         var fuel = 0
-        positions.forEach { fuel += kotlin.math.abs(it - median) }
+        positions.forEach { fuel += abs(it - median) }
         return fuel
     }
 
+    fun calculateFuelCostToCandidate(position: Int, target: Int): Int {
+        val distance = abs(target - position)
+        var fuelConsumption = 0
+        for(i in 1 .. distance) {
+            fuelConsumption += i
+        }
+        return fuelConsumption
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val positions = input[0].split(',').map { it.toInt() }.sorted()
+        val fuelCostPerMedianCandidate = mutableMapOf<Int, Int>()
+        for(candidate in positions[0] .. positions[positions.size - 1]) {
+            var candidateFuelCost = 0
+            positions.forEach {
+                candidateFuelCost += calculateFuelCostToCandidate(it, candidate)
+            }
+            fuelCostPerMedianCandidate[candidate] = candidateFuelCost
+        }
+        return fuelCostPerMedianCandidate.values.sorted()[0]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${dayNumber}_test")
     check(part1(testInput) == 37)
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day${dayNumber}")
     println("Part 1 answer: ${part1(input)}")
